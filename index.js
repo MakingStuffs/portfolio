bafc2411e4f5f1ef4a3a8a48018a8956b81d7670
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2020-10-02 00:09:13 +0100

    - remove http

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const app = require("./app");
 const logger = require('./utils/logger');
 const config = require("./utils/config");
-const http = require("http");
 
-const server = http.createServer(app);
-
-server.listen(config.PORT, () => {
+app.listen(config.PORT, () => {
   logger.info(`Connected on port ${config.PORT}`);
 });
