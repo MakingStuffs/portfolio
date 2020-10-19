069ab0a40c6e461f1122255400235a4f47e32fdf
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2020-10-19 08:50:33 +0100

    - Line 10: Added import to contact router - Line 21: Compression now uses the should compress function which has been moved into the mniddleware.js file - Line 22: Utilised the express.json() middleware - Line 27: Add the contact router middleware

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const config = require("./utils/config");
 const compression = require("compression"); 
 const express = require("express");
 const app = express();
@@ -8,31 +7,24 @@ const path = require("path");
 const favicon = require("serve-favicon");
 const indexRouter = require("./controllers/index");
 const pageRouter = require("./controllers/page");
+const contactRouter = require("./controllers/contact");
 const middleware = require("./utils/middleware");
-const logger = require("./utils/logger");
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "./public/"));
 
-app.use("/assets", express.static(path.join(__dirname, "public", "assets")));
-app.use(favicon(path.join(__dirname, "public", "assets", "img", "favicon.ico")));
 app.use(helmet());
 app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public", "assets")));
+app.use(favicon(path.join(__dirname, "public", "assets", "img", "favicon.ico")));
 app.use(middleware.requestLogger);
-app.use(compression({ filter: shouldCompress }))
+app.use(compression({ filter: middleware.shouldCompress }))
+app.use(express.json())
 
-function shouldCompress (req, res) {
-  if (req.headers['x-no-compression']) {
-    // don't compress responses with this request header
-    return false
-  }
-
-  // fallback to standard filter function
-  return compression.filter(req, res)
-}
 
 app.use(indexRouter);
 app.use("/page", pageRouter);
+app.use("/contact", contactRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
