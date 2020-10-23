8d9dbfb6d0280b64e24e1295123be66ffa15f475
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2020-10-23 13:28:59 +0100

    - Line 12: import config file - Line 13: Import crypto to make a random hash nonce - Line 15: Add the recaptcha key to local variables which will be passed to the EJS engine - Line 16: Add the nonce to the local variables - Lines 21 - 40: Set CSP -- Needs to be amended for production

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const compression = require("compression"); 
+const compression = require("compression");
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -9,18 +9,44 @@ const indexRouter = require("./controllers/index");
 const pageRouter = require("./controllers/page");
 const contactRouter = require("./controllers/contact");
 const middleware = require("./utils/middleware");
+const config = require("./utils/config");
+const crypto = require('crypto');
+
+app.locals.recaptcha_key = config.RECAPTCHA_KEY;
+app.locals.nonce = crypto.randomBytes(8).toString('hex');
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "./public/"));
 
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        "script-src-elem": ["'unsafe-inline'", "'self'", "https://www.google.com",(req, res) => `'nonce-${app.locals.nonce}'`],
+        "default-src": ["'self'", "https://www.google.com"],
+        "base-uri": ["'self'"],
+        "block-all-mixed-content": [],
+        "font-src": ["'self'", "https:", "data:"],
+        "frame-ancestors": ["'self'"],
+        "img-src": ["'self'", "data:"],
+        "object-src": ["none"],
+        "script-src": ["'unsafe-eval'", "'self'", "https://www.google.com"],
+        "frame-src": ["'self'", "https://www.google.com"],
+        "script-src-attr": ["none"],
+        "style-src": ["'self'", "https:", "'unsafe-inline'"],
+      },
+    },
+  })
+);
+
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public", "assets")));
-app.use(favicon(path.join(__dirname, "public", "assets", "img", "favicon.ico")));
+app.use(
+  favicon(path.join(__dirname, "public", "assets", "img", "favicon.ico"))
+);
 app.use(middleware.requestLogger);
-app.use(compression({ filter: middleware.shouldCompress }))
-app.use(express.json())
-
+app.use(compression({ filter: middleware.shouldCompress }));
+app.use(express.json());
 
 app.use(indexRouter);
 app.use("/page", pageRouter);
