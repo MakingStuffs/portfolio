dc6d74d2871353608dc14ffaef8fd4560cc29c9c
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2020-10-26 08:25:29 +0000

    - Added env variables for nodemailer and recaptcha

--- utils/config.js
+++ utils/config.js
@@ -3,9 +3,26 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 const SSL_KEY = process.env.SSL_KEY;
 const SSL_CERT = process.env.SSL_CERT;
+const RECAPTCHA_KEY = process.env.RECAPTCHA_KEY;
+const RECAPTCHA_URL = process.env.RECAPTCHA_URL;
+const RECAPTCHA_SECRET = process.env.RECAPTCHA_SECRET;
+const MAILER_USER = process.env.MAILER_USER;
+const MAILER_USER_PASS = process.env.MAILER_USER_PASS;
+const MAILER_HOST = process.env.MAILER_HOST;
+const MAILER_PORT = process.env.MAILER_PORT;
+const MAILER_RECIPIENT = process.env.MAILER_RECIPIENT;
+
 
 module.exports = {
   PORT,
   SSL_CERT,
-  SSL_KEY
+  SSL_KEY,
+  RECAPTCHA_KEY,
+  RECAPTCHA_URL,
+  RECAPTCHA_SECRET,
+  MAILER_USER,
+  MAILER_USER_PASS,
+  MAILER_RECIPIENT,
+  MAILER_PORT,
+  MAILER_HOST
 };
