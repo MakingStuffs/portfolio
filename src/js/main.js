e450a4293be05bb3bb4c41840ef941f959941189
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2021-05-26 19:20:10 +0100

    - Add regenerator runtime

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+// eslint-disable-next-line no-unused-vars
+import regeneratorRuntime from 'regenerator-runtime'
 import '../images/favicon.ico'
 import './modules/template-functions'
 import './modules/navigation-menu'
