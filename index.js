9f1845418e57453f95509faf0c4fdf8f9f5c8bd3
Paul Singh <33229318+MakingStuffs@users.noreply.github.com>
2021-05-26 19:01:54 +0100

    - remove SSL

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 const app = require('./app')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
-const spdy = require('spdy')
-const fs = require('fs')
 
-const secureServer = spdy.createServer({
-  key: fs.readFileSync(config.SSL_KEY),
-  cert: fs.readFileSync(config.SSL_CERT)
-}, app)
-
-secureServer.listen(config.PORT, () => {
-  logger.info(`Connected on port${secureServer.address().port}`)
+app.listen(config.PORT, () => {
+  logger.info(`Connected on port ${config.PORT}`)
 })
